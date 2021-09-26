9da8113f6167c180bcd1e414b1f6b82e9701d7ca
Hitakshi <ag37742@LC02Z805LLVCG.US.AD.WELLPOINT.COM>
2021-09-26 17:24:04 +0530

    minimize header rerenders

--- client/src/components/presentational/Header/index.tsx
+++ client/src/components/presentational/Header/index.tsx
@@ -1,14 +1,16 @@
-import React , {}from 'react';
+import React , {memo} from 'react';
 import './header.css'
 
 interface  HeaderProps {
   headerString: string
 }
-export const Header = ( { headerString }: HeaderProps ) : JSX.Element => {
-    return (
-      <div className="headerContainer">
-        <h1>{headerString} </h1>
-      </div>
-    )   
-}
-   
+
+export default memo (
+  function header ({ headerString }: HeaderProps ) : JSX.Element {
+      return (
+        <div className="headerContainer">
+          <h1>{headerString} </h1>
+        </div>
+      )   
+  }
+)
