82cdfff932a80688b8718bc84c8956255bde9e06
Hitakshi <ag37742@LC02Z805LLVCG.US.AD.WELLPOINT.COM>
2021-09-26 17:50:23 +0530

    add main app files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { ListWrapper } from './components/containers/ListWrapper'
+import Header from './components/presentational/Header';
+import { CarViewBox } from './components/presentational/CarViewBox';
+import useCarViewData from './components/customHooks/CarViewData/useCarViewData';
 import './App.css';
-import { Header } from './components/presentational/Header';
 
 export interface CarInterface {
   carId: string;
-  carPrice: number;
-  carMake: string;
-  carModel: string;
-  carImageUrl: string
+  carPrice?: number;
+  carMake?: string;
+  carModel?: string;
+  carImageUrl?: string
+  carDescription?: string;
 }
 
-function App() {
+function App() : JSX.Element {
+  const [ selectedCarId, setSelectedCarId] = useState<string>('');
+  var carViewBoxData: CarInterface | undefined = useCarViewData(selectedCarId) 
 
   return (
     <div className="App">
         <Header headerString= "CARS CATALOG"/>
-         <div className="bodyContainer">
-              <div className="leftContainer">
-              </div> 
-          
+          <div className="bodyContainer">
+            <div className="leftContainer">
+              <ListWrapper highlightedCar={selectedCarId} sendCarIdToParent={setSelectedCarId}/>
+            </div> 
+            { carViewBoxData &&
               <div className="rightContainer" >
+                <CarViewBox carData={carViewBoxData}/>
               </div>
-            
-        </div>
-    </div>
+            }   
+          </div>
+     </div>
   );
 }
 
